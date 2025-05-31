ba1efbd837dea5e364b2b42e2a0c0bf43e8872f4
Abai369 <jcaleb1110@gmail.com>
2025-05-30 20:37:53 -0700

    feat: Update Hero component styles and background image for enhanced visual appeal

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Hero.css';
+import bgImage from '../../assets/Background.png';
 
 const Hero = () => {
     return (
-        <section className='hero'>
+        <section className='hero'
+            style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${bgImage})`,
+            }}>
             <div className='hero__content'>
                 <h1 className="hero__title">Explore the Urban Fashion</h1>
                 <p className="hero__subtitle">Attitude and Style</p>
